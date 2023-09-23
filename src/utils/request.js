078625cc5e86baace694b75b149c8142a69ff35d
小章鱼鸭 <47490129+zykzhangyukang@users.noreply.github.com>
2023-09-23 23:11:35 +0800

    目录变更调整

--- src/utils/request.js
+++ src/utils/request.js
@@ -71,22 +71,13 @@ http.interceptors.response.use(
 
                 message.error(' 网关转发异常，请稍后再试！');
 
-            } else {
+            }else if (error.response.status === 500){
 
-                if (error.toString().search('timeout') > -1) {
-                    message.error('请求超时，请重试或联系管理员！');
-                }
+                message.error('抱歉，服务器内部异常！');
 
-                if (error.toString().search('Network Error') > -1) {
-                    message.error('请连接网络！');
-                } else {
-                    // 其他未知错误不提示
-                    message.error({
-                        content: error.message,
-                        type: 'error',
-                        duration: 5 * 1000
-                    })
-                }
+            }  else {
+
+                message.error('unknown error！');
             }
         }
 
